3169b42eabc942d1f666d5af047548ede02d3475
mr-abhi0 <durgapr09138@gmail.com>
2022-09-15 14:59:15 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    async function getAllStudents() {
+      try {
+        const students = await axios.get("http://127.0.0.1:8000/api/students/")
+        console.log(students.data)
+        setStudents(students.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getAllStudents()
+
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Connect Rest Api</h1>{
+
+        students.map((students, i) => {
+          return (
+            <h2> {students.id} {students.stuname} {students.email} </h2>
+          )
+        })
+      }
     </div>
   );
 }
